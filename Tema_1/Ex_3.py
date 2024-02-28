ae5a50cb806b496526a686311ee45c9c62a5424a
anaungurean <100145653+anaungurean@users.noreply.github.com>
2024-02-28 20:10:09 +0200

    Update Ex_3.py

--- Tema_1/Ex_3.py
+++ Tema_1/Ex_3.py
@@ -3,13 +3,7 @@ import numpy as np
 import math
 
 random_numbers = np.random.uniform(-np.pi/2, np.pi/2, 10000)
-print(random_numbers)
-errors_t_4 = []
-errors_t_5 = []
-errors_t_6 = []
-errors_t_7 = []
-errors_t_8 = []
-errors_t_9 = []
+min_errors = {'t_4': 0, 't_5': 0, 't_6': 0, 't_7': 0, 't_8': 0, 't_9': 0}
 
 for number in random_numbers:
     t_4 = (105*number - 10*number**3)/(105 - 45*number**2 + number**4)
@@ -19,4 +13,23 @@ for number in random_numbers:
     t_8 = (2027025*number - 270270*number**3 + 6930*number**5 - 36*number**7) / (2027025 - 945945*number**2 + 51975*number**4 - 630*number**6 + number**8)
     t_9 = (34459425*number - 4729725*number**3 + 135135*number**5 - 990*number**7 + number**9) / (34459425 - 16216200*number**2 + 945945*number**4 - 13860*number**6 + 45*number**8)
     tangenta = math.tan(number)
-    
+    errors = [abs(t_4 - tangenta), abs(t_5 - tangenta), abs(t_6 - tangenta), abs(t_7 - tangenta), abs(t_8 - tangenta), abs(t_9 - tangenta)]
+    print('For number:', number, 'the best approximation is:', errors.index(min(errors)) + 4, 'with the error:', min(errors))
+    min_errors['t_' + str(errors.index(min(errors)) + 4)] += 1
+
+
+sorted_errors = sorted(min_errors.items(), key=lambda x: x[1], reverse=True)
+
+print('Hierarchy of the best approximation:')
+index = 1
+for t, error in sorted_errors:
+    print(f'{index}. {t} with the minimum error for : {error} times.')
+    index += 1
+
+
+
+
+
+
+
+
